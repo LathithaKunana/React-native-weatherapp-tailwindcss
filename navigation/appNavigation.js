dc15e6692f9b17aad8e3b9ab5d01b33e23fb9a46
LathithaKunana <lathitha.kunana@younglings.africa>
2023-09-04 14:24:38 +0200

    added files

---
+++ navigation/appNavigation.js
@@ -0,0 +1,24 @@
+import { LogBox, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator;
+
+LogBox.ignoreLogs([
+    'Non-serializable values were found in the navigation state'
+])
+
+const AppNavigation = () => {
+  return (
+  < NavigationContainer>
+    <Stack.Navigator>
+        <Stack.Screen name="Home" options = {{headerShown: false}} component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  )
+}
+
+export default AppNavigation;
+
---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen;
+
+
